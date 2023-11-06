6bf6069a038f07e69a96e4c28e665e12166029a8
ikruus <ikerus393@gmail.com>
2023-11-06 10:43:56 +0100

    1

---
+++ Estructures/Excercicis/troba_maxim.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+
+"""
+troba_maxim.py rep números naturals de forma seqüencial i termina al rebre un de negatiu.
+Al terminar informa de quin ha estat el nombre més gran introduït.
+"""
+__author__   = "Xavi Quesada"
+__email__    = "f.javier.quesada@iesjoandaustria.org"
+__license__  = "GPL V3"
+
+def main():
+    return maxim
+
+if __name__ == "__main__":
+    print(f"El màxim ha estat: {main()}")
